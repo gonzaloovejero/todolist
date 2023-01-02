7f18586c322bec89e959ce12b4352b28576493c9
Gonzalo <gonzaovejero18@gmail.com>
2023-01-02 19:19:54 -0300

    first commit

---
+++ src/components/TaskCard.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
+
+export const TaskCard = ({ task }) => {
+  const { deleteTask } = useContext(TaskContext);
+
+  return (
+    <div className="bg-gray-800 text-white  p-4 rounded-md hover:cursor-default ">
+      <h1 className="text-xl font-bold capitalize">{task.title}</h1>
+      <p className="text-gray-500 text-sm">{task.description}</p>
+      <button
+        onClick={() => deleteTask(task.id)}
+        className="bg-red-500 px-2 py-1 rounded-md mt-4 hover:bg-red-400 "
+      >
+        Eliminar Tarea
+      </button>
+    </div>
+  );
+};
---
+++ src/data/tasks.js
@@ -0,0 +1,22 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "first task",
+    description: "this is my first task",
+  },
+  {
+    id: 2,
+    title: "second task",
+    description: "this is my task",
+  },
+  {
+    id: 3,
+    title: "third task",
+    description: "this is my task",
+  },
+  {
+    id: 4,
+    title: "quarter task",
+    description: "this is my task",
+  },
+];
